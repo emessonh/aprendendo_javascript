5f33178b11ba8de6acdff82471b1999ff32f789a
EMESSON HORACIO DOS SANTOS <2022005052@IFAL-MACEIO.INTRANET>
2023-10-10 17:34:11 -0300

    exercicio1_string_formatada

---
+++ exercicios/stringFormatada.js
@@ -0,0 +1,4 @@
+let nome = 'Emesson';
+let idade = 20;
+// console.log('Olá, meu nome é '+nome+' e eu tenho '+idade+' anos de idade!');
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos de idade!`);
