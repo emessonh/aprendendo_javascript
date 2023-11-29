3cce4cf51a3eb247ce267c1bf674d8132e737f70
EMESSON HORACIO DOS SANTOS <2022005052@IFAL-MACEIO.INTRANET>
2023-11-29 16:33:06 -0300

    conversoes em javascript

---
+++ conversoes.js
@@ -0,0 +1,35 @@
+// Conversões explícitas
+
+// 1 - Conversão para números:
+
+    // Método: Number(<valor a ser convertido>), String to Number e Boolean to Number
+
+    // False, empty strings e null = 0 
+    // True = 1
+    // qualquer número diferente de 0 = True
+    // Se não puder ser feita a conversão o resultado será NaN
+
+    // Métodos: parseInt(), parseFloat()
+    // Transforma em inteiro e ponto flutuante respectivamente
+
+// 2 - Conversão para String:
+
+    // Método: String(), toString()
+
+    // Ex's: Strint(True) -> 'True'
+    //       True.toString() -> 'True'
+    // obs: ao executar null.toString() ocorre um erro
+
+// 3 - Conversão para Boolean:
+
+    // Método: Boolean()
+
+    // os valores: '', 0, undefined, null, NaN geram um resultado False
+    // Qualquer outra coisa que não o informado acima gera True
+
+
+
+
+
+
+
