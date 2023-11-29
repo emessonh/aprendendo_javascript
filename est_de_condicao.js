1d473df13732a554c35c5356ee48313e5ec506ee
EMESSON HORACIO DOS SANTOS <2022005052@IFAL-MACEIO.INTRANET>
2023-11-29 16:59:33 -0300

    estruturas de condicao

---
+++ est_de_condicao.js
@@ -0,0 +1,29 @@
+// estrutura básica if...else
+
+// if (condicao){
+//     bloco de código
+// }
+/**
+ * else{
+ *     bloco de código
+ * }
+ */
+
+/**
+ * estrutura if...else if...else
+ * 
+ * if (condicao){
+ *      bloco de código
+ * }
+ * 
+ * else if (condicao){
+ *      bloco de código
+ * }
+ * 
+ * else{
+ *      bloco de código
+ * }
+ */
+
+// obs: se o bloco de código houver apenas uma linha os {} podem ser ocultados
+
