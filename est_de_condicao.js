e36107a5e1e5b272ddbf517cb8c73cfe3ec7aadb
EMESSON HORACIO DOS SANTOS <2022005052@IFAL-MACEIO.INTRANET>
2023-11-29 17:28:34 -0300

    switch case e operador ternario

--- est_de_condicao.js
+++ est_de_condicao.js
@@ -1,29 +1,65 @@
 // estrutura básica if...else
 
-// if (condicao){
-//     bloco de código
-// }
-/**
- * else{
- *     bloco de código
- * }
- */
+    // if (condicao){
+    //     bloco de código
+    // }
+    /**
+     * else{
+     *     bloco de código
+     * }
+     */
 
 /**
  * estrutura if...else if...else
+    * 
+    * if (condicao){
+    *      bloco de código
+    * }
+    * 
+    * else if (condicao){
+    *      bloco de código
+    * }
+    * 
+    * else{
+    *      bloco de código
+    * }
+*/
+
+// obs: se o bloco de código houver apenas uma linha os {} podem ser ocultados
+
+// Operador ternário: codicao ? expressao 1 : expressao 2
+
+    // A expressão 1 será executada quando o resultado for verdadeiro
+    // A expressão 2 será executada quando o resultado for falso
+
+// Switch case:
+
+// Sintaxe básica: 
+
+/**
+ * switch (variavel/expressao){
+ *      case 1:
+ *          código para caso 1
+ *          break;
+ *      case 2:
+ *          código para caso 2
+ *          break;
  * 
- * if (condicao){
- *      bloco de código
+ *      default:
+ *          código default
  * }
  * 
- * else if (condicao){
- *      bloco de código
- * }
+ * obs: o caso default é opcional
  * 
- * else{
- *      bloco de código
+ * switch case com múltiplos casos:
+ * 
+ * switch (variavel/expressao){
+ *      case 1:
+ *      case 2:
+ *      case 3:
+ *          código para caso 1 ou caso 2 ou caso 3
  * }
  */
 
-// obs: se o bloco de código houver apenas uma linha os {} podem ser ocultados
+
 
