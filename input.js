6ac5d382ae954c55a464861e3c4a739db184f20b
emessonh <ehs4@aluno.ifal.edu.br>
2023-12-01 17:12:23 -0300

    input e loops

---
+++ input.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')(); // importa o prompt-sync e inicializá-o
+
+let n = prompt('Digite um número: ');
+console.log(`Seu número é ${n}`);
---
+++ loop.js
@@ -0,0 +1,38 @@
+// Estrutura básica for
+
+/**
+ * for (inicializa contador; condicao; atualizacao contador){
+ *      bloco de codigo
+ * }
+ */
+
+// Usado quando se sabe a quantidade de repetições da iteração
+
+//  Estrutura básica While
+ 
+/* 
+ * while (condicao){
+ *      bloco de código
+ * }
+ */
+
+// Estrutura básica do...while
+
+/**
+ * do{
+ *      bloco de codigo
+ * }while(condicao)
+ */
+
+/**
+ * Executa o código a primeira vez independente da situação,
+ * em seguida avalia a condição, caso seja verdadeira repete o loop,
+ * sendo falsa sai do laço de repetição.
+ */
+
+// Usadas quando não se sabe a quantidade de iterações, do...while e while
+
+ 
+
+
+
