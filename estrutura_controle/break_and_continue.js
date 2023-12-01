880ea11ebd824112846b5c3a6adf65a63016a812
emessonh <ehs4@aluno.ifal.edu.br>
2023-12-01 18:17:41 -0300

    break e continue

---
+++ estrutura_controle/break_and_continue.js
@@ -0,0 +1,10 @@
+// palavra chave break
+
+/* para o laço de repetição, em caso de estruturas de repetição aninhadas,
+   o laço que contem o break que será afetado.
+*/
+
+// palavra chave continue
+
+// Volta ao início da estrutura de repetição, em caso de estruturas de repetição aninhadas,
+// a que contém o 'continue' que é afetada
