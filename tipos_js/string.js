9647ce13b28e26f854eb6914e30268eb61aa68d1
emessonh <ehs4@aluno.ifal.edu.br>
2023-12-02 20:16:59 -0300

    string

---
+++ tipos_js/string.js
@@ -0,0 +1,10 @@
+// string: conjunto de caracteres
+
+// Usada com "", '' ou `` (utilizada para interpolação)
+
+// usando múltiplas linhas '+' ou '\'
+
+// caracter de escape '\'
+
+// obs: strings são imutáveis
+
